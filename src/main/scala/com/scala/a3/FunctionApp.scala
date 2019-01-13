fe7cc74a2ad009a65d6d56f156c8df3b3a669657
lihaocheng <lihaocheng00@126.com>
2019-01-13 23:51:55 +0800

    function

--- src/main/scala/com/scala/a3/FunctionApp.scala
+++ src/main/scala/com/scala/a3/FunctionApp.scala
@@ -5,6 +5,16 @@ object FunctionApp {
     println(add(2,3))
     //没有入参的函数调用时可以直接写函数名称，括号可省略。
     println(three)
+    sayhello
+    sayname()
+    sayname("lhc")
+    println(speed(1,10))
+    //改变了传入参数的顺序
+    println(speed(time=1,distance = 10))
+    println(sum(1,3))
+    println(sum1(1,3,4))
+    println(sum1(1,3,4,6,7))
+
   }
 
   def add(x:Int,y:Int):Int={
@@ -14,5 +24,33 @@ object FunctionApp {
   }
 
   def three()=1+2
+
+  def sayhello():Unit={
+    println("hello")
+
+  }
+
+  def sayname(name:String = "PK"):Unit={
+    println(name)
+  }
+
+  def loadConf(name:String="" ): Unit ={
+
+  }
+  def speed(distance:Float,time:Float):Float={
+    distance/time
+  }
+  def sum(a:Int,b:Int):Int={
+    a+b
+  }
+
+  def sum1(number:Int*)={
+    var result=0
+    for (number<-number){
+      result+=number
+    }
+    result
+  }
+
 
 }
